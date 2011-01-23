353335b3a18ca09f8d25b452284a5369ed220015
Jon Rowe <hello@jonrowe.co.uk>
2011-01-23 23:39:32 +0000

    model setup steps

--- features/step_definitions/model_steps.rb
+++ features/step_definitions/model_steps.rb
@@ -4,6 +4,11 @@ end
 
 Given /^my model has a attribute named "([^"]*)"$/ do |field_name|
   @model.class.class_eval do
-    define_method(field_name) { "a value for checking later" }
+    attr_accessor field_name
   end
+  @model.send(:"#{field_name}=","a value for checking later")
+end
+
+Given /^my model's "([^"]*)" attribute is nil$/ do |field_name|
+  @model.send(:"#{field_name}=", nil)
 end
