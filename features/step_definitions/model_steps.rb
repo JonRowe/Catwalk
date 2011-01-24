7ca457aababf962d94a83d07f142b8f3d8548ac0
Jon Rowe <hello@jonrowe.co.uk>
2011-01-24 22:19:23 +0000

    add in optional value specifier

--- features/step_definitions/model_steps.rb
+++ features/step_definitions/model_steps.rb
@@ -2,11 +2,11 @@ Given "I have an ActiveModel based model" do
   @model = ActiveModelExample.new
 end
 
-Given /^my model has a attribute named "([^"]*)"$/ do |field_name|
+Given /^my model has an attribute named "([^"]*)"(?: with a value "([^"]*)")?$/ do |field_name,field_value|
   @model.class.class_eval do
     attr_accessor field_name
   end
-  @model.send(:"#{field_name}=","a value for checking later")
+  @model.send(:"#{field_name}=",field_value || "a value for checking later")
 end
 
 Given /^my model's "([^"]*)" attribute is nil$/ do |field_name|
