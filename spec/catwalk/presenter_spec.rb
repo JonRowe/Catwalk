ea1a4d02d2599c1b221692c6039ec7097e68bc81
Jon Rowe <hello@jonrowe.co.uk>
2014-07-03 21:45:21 +1000

    eliminate a subject

--- spec/catwalk/presenter_spec.rb
+++ spec/catwalk/presenter_spec.rb
@@ -5,17 +5,15 @@ module Catwalk
 
     let(:model) { double "model" }
     let(:klass) { Class.new }
-    let(:presenter) { klass.new(model) }
+    subject(:presenter) { klass.new(model) }
 
     before do
       klass.class_eval { include Catwalk::Presenter }
     end
 
     describe "initialization" do
-      subject { presenter }
-
       it "should assign an the model to an instance variable accessible via to_model" do
-        subject.to_model.should == model
+        presenter.to_model.should == model
       end
     end
 
@@ -27,7 +25,6 @@ module Catwalk
       before do
         model_name.stub(:kind_of?).with(String).and_return(true)
       end
-      subject { presenter }
 
       it_should_behave_like "ActiveModel"
       it_should_behave_like "proxy active_model method", :to_key,     :from => :model
@@ -40,14 +37,12 @@ module Catwalk
     end
 
     describe "access_as" do
-      subject do
+      it "adds an accessors with the specified name" do
         klass.class_eval do
           access_as :model_accessor
         end
-        presenter
+        presenter.should == model
       end
-
-      its(:model_accessor) { should == model }
     end
 
     describe "field" do
@@ -55,36 +50,27 @@ module Catwalk
 
       before do
         model.stub(:a_field).and_return(model_value)
-      end
-
-      subject do
         klass.class_eval do
           field :a_field
         end
-        presenter.a_field
       end
 
       it "should proxy field to model" do
         model.should_receive(:a_field).and_return(model_value)
-        subject
+        presenter.a_field.should eq model_value
       end
-      it { should == model_value }
 
       context "when default_when is specified" do
         before do
           model_value.stub(:method_is_true).and_return(false)
-        end
-
-        subject do
           klass.class_eval do
             field :a_field, :default_when => :method_is_true
           end
-          presenter.a_field
         end
 
         it "should check wether method passes on field" do
           model_value.should_receive(:method_is_true).and_return false
-          subject
+          presenter.a_field
         end
 
         context "and default_when check fails" do
@@ -95,25 +81,21 @@ module Catwalk
           it { should == "Not Set" }
         end
       end
+
       context "when format_with block is specified" do
         let(:formatted_value) { "nicer value" }
 
         before do
           model_value.stub(:my_format_method).and_return formatted_value
-        end
-
-        subject do
           klass.class_eval do
             field :a_field, :format_with => lambda { |field| field.my_format_method }
           end
-          presenter.a_field
         end
 
         it "should format the field with the block" do
           model_value.should_receive(:my_format_method).and_return formatted_value
-          subject
+          presenter.a_field.should eq formatted_value
         end
-        it { should == formatted_value }
       end
     end
   end
